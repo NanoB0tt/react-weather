99a340830e35220340573effb4fbde1ba586c617
NanoB0tt <marianofassikoropecki@gmail.com>
2022-11-30 11:12:33 -0300

    added new feature

--- src/screens/WeatherApp.tsx
+++ src/screens/WeatherApp.tsx
@@ -3,15 +3,18 @@ import { LatLon } from "../interfaces/interfaces";
 import CityWeather from "../components/CityWeather/CityWeather";
 import SearchBar from "../components/SearchBar/SearchBar";
 import WeekWeather from "../components/WeekWeather/WeekWeather";
+import DisplayOptions from "../components/DisplayOptions/DisplayOptions";
 
 const WeatherApp = () => {
   const [location, setLatLon] = useState<LatLon | null>(null);
+  const [option, setOption] = useState('day');
 
   return (
     <>
       <SearchBar setLatLon={setLatLon} />
-      <CityWeather location={location} />
-      <WeekWeather location={location} />
+      {location && <DisplayOptions setOption={setOption} />}
+      <CityWeather location={location} option={option} />
+      <WeekWeather location={location} option={option} />
     </>
   )
 }
