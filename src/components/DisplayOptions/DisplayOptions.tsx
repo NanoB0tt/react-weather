8b1c2f88e4981f58765e861d61bdb37bf0c44635
NanoB0tt <marianofassikoropecki@gmail.com>
2022-11-30 23:56:14 -0300

    added DisplayOptions styles

--- src/components/DisplayOptions/DisplayOptions.tsx
+++ src/components/DisplayOptions/DisplayOptions.tsx
@@ -1,15 +1,22 @@
-import { Dispatch, SetStateAction } from "react";
+import { Dispatch, SetStateAction, useState } from "react";
+import style from "./DisplayOptions.module.css";
 
 interface Props {
   setOption: Dispatch<SetStateAction<string>>
 }
 
 const DisplayOptions = ({ setOption }: Props) => {
+  const [isPressed, setIsPressed] = useState(true);
+
+  const buttonAction = (str: string, bool: boolean) => {
+    setOption(str);
+    setIsPressed(bool);
+  }
 
   return (
-    <div>
-      <button onClick={() => setOption('day')}>Day</button>
-      <button onClick={() => setOption('week')}>Week</button>
+    <div className={style["btn-container"]}>
+      <button className={`${style["btn-day"]} ${isPressed ? style["pressed"] : ""}`} onClick={() => buttonAction('day', true)}>Day</button>
+      <button className={`${style["btn-week"]} ${!isPressed ? style["pressed"] : ""}`} onClick={() => buttonAction('week', false)}>Week</button>
     </div>
   )
 }
