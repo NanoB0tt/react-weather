b7724cf2590917be551ec54b04dfd08a6db1fc86
NanoB0tt <marianofassikoropecki@gmail.com>
2023-03-03 09:52:36 -0300

    fixed http request

--- src/services/getCity.ts
+++ src/services/getCity.ts
@@ -1,7 +1,7 @@
 import { City } from "../interfaces/interfaces";
 
 export const getCity = async (cityName: string): Promise<City[]> => {
-  const response = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=5&appid=${import.meta.env.VITE_WEATHER_API_KEY}`);
+  const response = await fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=5&appid=${import.meta.env.VITE_WEATHER_API_KEY}`);
   const data = await response.json();
   return data;
 }
